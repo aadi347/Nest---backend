e378b299ae303226b5757b2de29075fb1369da9e
Aditya Kumar <aadi678a@gmail.com>
2025-02-19 13:06:50 +0530

    api listed

---
+++ api.js
@@ -0,0 +1,11 @@
+// signup route {post} - http://localhost:3000/api/auth/signup
+// login route {post} - http://localhost:3000/api/auth/login
+// logout route {get} - http://localhost:3000/api/auth/logout
+// createBlog route {post} - http://localhost:3000/api/blog/createBlog
+// getBlogs route {get} - http://localhost:3000/api/blog/getBlogs
+// getBlogById route {get} - http://localhost:3000/api/blog/getBlogById/:id
+// registerflat route {post} - http://localhost:3000/api/flats/registerFlat
+// getallflats route {get} - http://localhost:3000/api/flats/getAllFlats
+// getflatbyid route {get} - http://localhost:3000/api/flats/getFlatById/:id
+// updateflat route {put} - http://localhost:3000/api/flats/updateFlat/:id
+// deleteflat route {delete} - http://localhost:3000/api/flats/deleteFlat/:id
